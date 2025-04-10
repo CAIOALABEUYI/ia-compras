df8ce7932b44d4e3ece9e601ed2bc49391395d5d
CAIOALABEUYI <caioadoliveira08@gmail.com>
2025-04-10 02:56:21 -0300

    Update ia_compras_pro_v6.py

--- ia_compras_pro_v6.py
+++ ia_compras_pro_v6.py
@@ -166,4 +166,4 @@ with aba1:
     st.plotly_chart(fig_gastos_pizza)
 
     st.subheader("Avaliação de Fornecedores")
-    fig_fornecedores_scatter = px.scatter(data_forne)
+    fig_fornecedores_scatter = px.scatter(data_forne
